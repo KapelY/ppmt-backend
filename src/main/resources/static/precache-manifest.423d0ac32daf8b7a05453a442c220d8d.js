d0348ea146de1f54599341a254aab79d2b5ef7a4
Kapelushny <zig.tos@gmail.com>
2019-10-17 14:04:57 +0300

    react

---
+++ src/main/resources/static/precache-manifest.423d0ac32daf8b7a05453a442c220d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d692dd55ce8899a8d1b88cfb03167d8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1478778e84216422a97",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "61a2e5eef7a3609b3dea",
+    "url": "/static/css/main.7fa22151.chunk.css"
+  },
+  {
+    "revision": "e1478778e84216422a97",
+    "url": "/static/js/2.73054d2d.chunk.js"
+  },
+  {
+    "revision": "61a2e5eef7a3609b3dea",
+    "url": "/static/js/main.ebb3572c.chunk.js"
+  },
+  {
+    "revision": "48ed8f1f6f86543ce515",
+    "url": "/static/js/runtime-main.2e6edbb3.js"
+  }
+]);
